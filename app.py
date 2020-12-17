9b448173750c9fc72d84e93eecca4530ac46622a
XUAN LI <xuanli@XUANs-MacBook-Air.local>
2020-12-17 18:30:11 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask 
+from flask import request, render_template
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "/usr/src/app/static"
+
+@app.route("/", methods=["GET", "POST"])
+def upload_predict():
+    if request.method == "POST":
+        image_file = request.files["image"]
+        if image_file:
+            image_location = os.path.join(UPLOAD_FOLDER, 
+                                image_file.filename)
+            image_file.save(image_location)
+            return render_template("index.html", image_loc=image_file.filename)
+    return render_template("index.html", image_loc=None)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=12000, debug=True)
